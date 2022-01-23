cd8e6b780d5b975eba1173d30e8072137ad96f8c
Sourabh Ghorpade <sourabh.ghorpade@gmail.com>
2022-01-23 12:34:39 +0530

    Move charge and points calculation into rental/

--- src/main/java/Customer.java
+++ src/main/java/Customer.java
@@ -3,12 +3,6 @@ import java.util.Iterator;
 import java.util.List;
 
 public class Customer {
-  public static final int REGULAR_MOVIE_BASE_RENTAL = 2;
-  public static final double CHILDRENS_MOVIE_BASE_CHARGE = 1.5;
-  public static final int NEW_RELEASE_FLAT_CHARGE = 3;
-  public static final int REGULAR_MOVIE_BASE_RENTAL_PERIOD_IN_DAYS = 2;
-  public static final int CHILDRENS_MOVIE_BASE_RENTAL_PERIOD_IN_DAYS = 3;
-  public static final double ADDITIONAL_CHARGE_PER_DAY = 1.5;
   private final String _name;
   private final List<Rental> _rentals = new ArrayList<>();
 
@@ -32,8 +26,8 @@ public class Customer {
 
     while (rentals.hasNext()) {
       Rental rental = rentals.next();
-      double rentalChargeForThisRental = chargeForRental(rental);
-      frequentRenterPoints += getFrequentRenterPoints(rental);
+      double rentalChargeForThisRental = rental.charge();
+      frequentRenterPoints += rental.getFrequentRenterPoints();
       invoiceStatement.append(generateInvoiceLineForRental(rental, rentalChargeForThisRental));
       totalInvoiceAmount += rentalChargeForThisRental;
     }
@@ -53,37 +47,5 @@ public class Customer {
 
   private String generateInvoiceLineForRental(Rental rental, double rentalChargeForThisRental) {
     return "\t" + rental.getMovie().getTitle() + "\t" + rentalChargeForThisRental + "\n";
-  }
-
-  private int getFrequentRenterPoints(Rental rental) {
-    int frequentRenterPoints = 1;
-
-    // add bonus for a two day new release rental
-    if (rental.getMovie().getPriceCode() == Movie.NEW_RELEASE && rental.getDaysRented() > 1)
-      frequentRenterPoints++;
-    return frequentRenterPoints;
-  }
-
-  private double chargeForRental(Rental rental) {
-    double rentalCharge = 0;
-    // determines the amount for each line
-    switch (rental.getMovie().getPriceCode()) {
-      case Movie.REGULAR -> {
-        rentalCharge += REGULAR_MOVIE_BASE_RENTAL;
-        rentalCharge = getRentalChargeForBeyondBasePeriod(rental.getDaysRented(), rentalCharge, REGULAR_MOVIE_BASE_RENTAL_PERIOD_IN_DAYS);
-      }
-      case Movie.NEW_RELEASE -> rentalCharge += rental.getDaysRented() * NEW_RELEASE_FLAT_CHARGE;
-      case Movie.CHILDRENS -> {
-        rentalCharge += CHILDRENS_MOVIE_BASE_CHARGE;
-        rentalCharge = getRentalChargeForBeyondBasePeriod(rental.getDaysRented(), rentalCharge, CHILDRENS_MOVIE_BASE_RENTAL_PERIOD_IN_DAYS);
-      }
-    }
-    return rentalCharge;
-  }
-
-  private double getRentalChargeForBeyondBasePeriod(int daysRented, double rentalCharge, int baseRentalPeriodInDays) {
-    if (daysRented > baseRentalPeriodInDays)
-      rentalCharge += (daysRented - baseRentalPeriodInDays) * ADDITIONAL_CHARGE_PER_DAY;
-    return rentalCharge;
   }
 }
--- src/main/java/Rental.java
+++ src/main/java/Rental.java
@@ -1,4 +1,12 @@
 class Rental {
+  public static final int MINIMUM_RENT_PERIOD_FOR_BONUS_POINTS = 1;
+  public static final int REGULAR_MOVIE_BASE_RENTAL = 2;
+  public static final double CHILDRENS_MOVIE_BASE_CHARGE = 1.5;
+  public static final int NEW_RELEASE_FLAT_CHARGE = 3;
+  public static final int REGULAR_MOVIE_BASE_RENTAL_PERIOD_IN_DAYS = 2;
+  public static final int CHILDRENS_MOVIE_BASE_RENTAL_PERIOD_IN_DAYS = 3;
+  public static final double ADDITIONAL_CHARGE_PER_DAY = 1.5;
+
   private final Movie _movie;
   private final int _daysRented;
 
@@ -13,5 +21,39 @@ class Rental {
 
   public Movie getMovie() {
     return _movie;
+  }
+
+  public int getFrequentRenterPoints() {
+    int frequentRenterPoints = 1;
+    if (isEligibleForBonus()) {
+      frequentRenterPoints++;
+    }
+    return frequentRenterPoints;
+  }
+
+  private boolean isEligibleForBonus() {
+    return getMovie().getPriceCode() == Movie.NEW_RELEASE && getDaysRented() > MINIMUM_RENT_PERIOD_FOR_BONUS_POINTS;
+  }
+
+  double charge() {
+    double rentalCharge = 0;
+    switch (getMovie().getPriceCode()) {
+      case Movie.REGULAR -> {
+        rentalCharge += REGULAR_MOVIE_BASE_RENTAL;
+        rentalCharge = getRentalChargeForBeyondBasePeriod(getDaysRented(), rentalCharge, REGULAR_MOVIE_BASE_RENTAL_PERIOD_IN_DAYS);
+      }
+      case Movie.NEW_RELEASE -> rentalCharge += getDaysRented() * NEW_RELEASE_FLAT_CHARGE;
+      case Movie.CHILDRENS -> {
+        rentalCharge += CHILDRENS_MOVIE_BASE_CHARGE;
+        rentalCharge = getRentalChargeForBeyondBasePeriod(getDaysRented(), rentalCharge, CHILDRENS_MOVIE_BASE_RENTAL_PERIOD_IN_DAYS);
+      }
+    }
+    return rentalCharge;
+  }
+
+  private double getRentalChargeForBeyondBasePeriod(int daysRented, double rentalCharge, int baseRentalPeriodInDays) {
+    if (daysRented > baseRentalPeriodInDays)
+      rentalCharge += (daysRented - baseRentalPeriodInDays) * ADDITIONAL_CHARGE_PER_DAY;
+    return rentalCharge;
   }
 }
